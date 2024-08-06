cbe2ac45b22209d67309f0bcf09a24259508ed56
Devix-old <mohamed.elgharef@gmail.com>
2024-08-06 17:39:32 +0100

    updates

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let string = "";
-  for (let i = 0; i <= reportWithIterator.length - 1; i++) {
-    string += reportWithIterator[i];
-    if (i < reportWithIterator.length - 1) {
-      string += " | ";
-    }
-  }
-  return string;
+  return reportWithIterator.join(' | ');
 }
