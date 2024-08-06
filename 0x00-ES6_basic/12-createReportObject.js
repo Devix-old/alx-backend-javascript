45187a0eb117b72cb75dcd32bd8dd7bd22fd0a4f
Devix-old <mohamed.elgharef@gmail.com>
2024-08-06 17:02:49 +0100

    updates

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(x) {
+      return Object.keys(x).length;
+    },
+  }
+}
