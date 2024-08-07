7d0b328acdfeb64ab1df21ba142d1791a9fe7bcd
Devix-old <mohamed.elgharef@gmail.com>
2024-08-07 16:05:55 +0100

    :

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
-export default function handleResponseFromAPI(promise){
+export default function handleResponseFromAPI(promise) {
   promise
-    .then("hello")
-    .then(console.log("Got a response from the API"))
-    .catch(new Error())
+    .then({status: 200, body: success})
+    .then(console.log('Got a response from the API'))
+    .catch(new Error());
 }
